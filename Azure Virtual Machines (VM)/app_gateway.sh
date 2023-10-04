775d0026eb8560825a597a2a6f708500781ab4a8
Joan Amengual <joan@consilientlabs.com>
2023-10-04 13:28:57 +0200

    app gateway

---
+++ Azure Virtual Machines (VM)/app_gateway.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir /var/www/html/imagen/index.html
+echo "Esto son mis imagenes" > /var/www/html/imagen/index.html
+
+mkdir /var/www/html/video/index.html
+echo "Esto son mis videos" > /var/www/html/video/index.html
