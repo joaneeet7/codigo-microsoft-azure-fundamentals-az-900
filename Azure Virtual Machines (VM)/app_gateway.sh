e502cf4df4b63aef8f9ba5dc94f1090e88f48ccd
Joan Amengual <joan@consilientlabs.com>
2023-10-04 13:51:51 +0200

    app gateway

--- Azure Virtual Machines (VM)/app_gateway.sh
+++ Azure Virtual Machines (VM)/app_gateway.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-mkdir /var/www/html/imagen
-echo "Esto son mis imagenes" > /var/www/html/imagen/index.html
+# Adjuntar estos comandos a la VM de imagen
+sudo mkdir /var/www/html/imagen
+sudo sh -c 'echo "Esto son mis imagenes" > /var/www/html/imagen/index.html'
 
-mkdir /var/www/html/video
-echo "Esto son mis videos" > /var/www/html/video/index.html
+# Adjuntar estos comandos a la VM de video
+sudo mkdir /var/www/html/video
+sudo sh -c 'echo "Esto son mis videos" > /var/www/html/video/index.html'
